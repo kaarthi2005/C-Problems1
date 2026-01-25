1a2b85a046113a910a3358d9b65361626ad89f14
Kaarthi_S <kaarthisenthilkumar@gmail.com>
2026-01-25 18:15:46 +0530

    Add files via upload
     program to swap two numbers using a temporary variable.
    in C

---
+++ probelm 12.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+    int num1,num2,temp;
+
+    scanf("%d  %d",&num1 ,&num2);
+
+    temp = num1;
+
+    num1 = num2;
+
+    num2 = temp;
+
+    printf("%d  %d",num1,num2);
+
+
+    return 0;
+}

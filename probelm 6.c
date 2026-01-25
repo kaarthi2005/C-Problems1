8a86242c7214c4f3a117cbdb975f496cde70ff60
Kaarthi_S <kaarthisenthilkumar@gmail.com>
2026-01-25 17:12:45 +0530

    Add files via upload
     program to calculate the circumference of a circle .in C

---
+++ probelm 6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+
+    int radius ;
+    scanf("%d",&radius);
+
+    float circumference_of_circle = 2 * 3.14 *radius;
+
+    printf("%0.1f",circumference_of_circle);
+
+    return 0;
+}

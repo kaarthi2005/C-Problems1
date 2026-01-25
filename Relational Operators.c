e6ec91cb7ab0cc3a64be5ba39f6461c51336de86
Kaarthi_S <kaarthisenthilkumar@gmail.com>
2026-01-25 12:55:25 +0530

    Add files via upload
    Topic: Relational Operators (> < == != >= <=)

---
+++ Relational Operators.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+
+int main (){
+
+    //1. Compare two numbers using >.
+    int a =10;
+    int b=5;
+
+    printf("%d",a>b);
+
+
+     int a =3;
+    int b=8;
+
+    printf("%d",a>b);
+
+
+    int a =7;
+    int b=7;
+
+    printf("%d",a>b);
+
+    //2.Compare two numbers using <
+
+    int a =5;
+    int b=10;
+
+    printf("%d",a<b);
+
+
+     int a =9;
+    int b=2;
+
+    printf("%d",a<b);
+
+
+    int a =4;
+    int b=4;
+
+    printf("%d",a<b);
+
+
+
+
+
+    //3.Compare two numbers using ==
+
+    int a =5;
+    int b=5;
+
+    printf("%d",a==b);
+
+
+     int a =3;
+    int b=4;
+
+    printf("%d",a==b);
+
+
+    int a =0;
+    int b=0;
+
+    printf("%d",a==b);
+
+
+
+
+
+    //4.Compare two numbers using !=
+
+    int a =5;
+    int b=3;
+
+    printf("%d",a!=b);
+
+
+     int a =4;
+    int b=4;
+
+    printf("%d",a!=b);
+
+
+    int a =9;
+    int b=1;
+
+    printf("%d",a!=b);
+
+
+
+    
+    //5.Compare two numbers using >= and <=
+
+    int a =5;
+    int b=5;
+
+    printf("%d  %d",a>=b,a<=b);
+
+
+    int a =10;
+    int b=5;
+
+    printf("%d  %d",a>=b,a<=b);
+
+
+    int a =3;
+    int b=7;
+
+    printf("%d  %d",a>=b,a<=b);
+
+
+
+    
+
+    return 0;
+
+}

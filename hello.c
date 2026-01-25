e957af2ffc7c4ce5465dc5626277d306af44fe87
Kaarthi_S <kaarthisenthilkumar@gmail.com>
2026-01-25 11:13:52 +0530

    Add files via upload
    Topic 1: Printing Statements – Test Cases

--- hello.c
+++ hello.c
@@ -1,7 +1,75 @@
 #include <stdio.h>
 
 int main (){
+
+    //printing a name
     printf("Hello World");
+    printf("Sivan");
+    printf("ravi");
+
+    printf("\n");
+    //printing a integer valve
+    int a = 10;
+    int b = 25;
+
+    printf("%d",a);
+    printf("%d",b);
+
+    printf("\n");
+
+    //Print a floating-point number
+
+    float a1 = 3.5;
+    float b2 = 7.25;
+
+    printf("%0.1f",a1);
+    printf("%0.2f",b2);
+
+     printf("\n");
+
+    //Print a character
+
+    char a3 = 'A';
+    char b3 = 'Z';
+
+    printf("%c",a3);
+    printf("%c",b3);
+
+    printf("\n");
+
+    //Print two integers in one line
+
+    int a4 = 5;
+    int b4 = 10;
+
+    printf("%d %d",a4,b4);
+
+
+    int a5 = 20;
+    int b5 = 30;
+
+    printf("%d %d",a5,b5);
+
+    printf("\n");
+
+
+    //Print a string along with a number
+
+    int c = 20;
+    int d = 5;
+
+    printf("Age:%d",c);
+    printf("Count:5",d);
+
+    printf("\n");
+
+    //Print output in two lines using \n
+
+    printf("Hello \n World");
+     printf("\n");
+    printf("Good \n Morning");
+
+
 
     return 0 ;
 }

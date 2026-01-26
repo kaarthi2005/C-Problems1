d140e5630901c60d16f0678e8393abe04f7e5679
Kaarthi_S <kaarthisenthilkumar@gmail.com>
2026-01-26 11:18:43 +0530

    Add files via upload
    22. Day of Week in C

---
+++ if-conditionprobelm22.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main (){
+    printf("Enter a number : ");
+    int num;
+    scanf("%d",&num);
+
+    switch (num)
+    {
+    case 1:
+        printf("Sunday");
+        break;
+    case 2:
+        printf("Monday");
+        break;
+    case 3:
+        printf("Tuesday");
+        break;
+    case 4:
+        printf("Wednesday");
+        break;
+    case 5:
+        printf("Thursday");
+        break;
+    case 6:
+        printf("Friday");
+        break;
+    case 7:
+        printf("Saturday");
+        break;
+    
+    default:
+        break;
+    }
+
+    return 0;
+}

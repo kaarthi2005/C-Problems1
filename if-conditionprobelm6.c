a984d0fff7e5b7491b983c04ad8f15d17a034fc0
Kaarthi_S <kaarthisenthilkumar@gmail.com>
2026-01-26 11:08:53 +0530

    Add files via upload
    6. Vowel or Consonant in C

---
+++ if-conditionprobelm6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    scanf("%c", &ch);
+
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+        ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+        printf("%c is an Vowel", ch);
+    } else {
+        printf("%c is an Consonant", ch);
+    }
+
+    return 0;
+}

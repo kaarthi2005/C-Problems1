6d47ec439136f4d37ccd62c823571a8f2aa15d6c
Kaarthi_S <kaarthisenthilkumar@gmail.com>
2026-01-26 11:07:57 +0530

    Add files via upload
    4. Find Greater of Two Numbers in C

---
+++ if-condition probelm4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (){
+    int num1,num2 ;
+    scanf("%d  %d",&num1,&num2);
+
+    if(num1 > num2){
+        printf("%d is Greater",num1);
+    }
+    else {
+          printf("%d is Greater",num2);
+    }
+
+    return 0 ;
+
+}

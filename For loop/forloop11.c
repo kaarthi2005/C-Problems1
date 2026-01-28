351061bd86fe76d652c67031ae591d370e4c891d
Kaarthi_S <kaarthisenthilkumar@gmail.com>
2026-01-28 22:15:21 +0530

    Add files via upload

---
+++ For loop/forloop11.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    
+    
+    int sum =0;
+    for( ;n>0; n = n/10){
+        sum += n%10;
+    }
+    printf("%d",sum);
+
+    return 0;
+}

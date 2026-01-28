c66c4bbd2abe971daedeaab4cbab65f1b4b68704
Kaarthi_S <kaarthisenthilkumar@gmail.com>
2026-01-28 22:13:33 +0530

    Add files via upload

---
+++ For loop/forloop6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int sum =1;
+
+    for(int i =1; i<= n; i++){
+        
+           sum = sum*i;
+        
+    }
+    printf("%d",sum);
+
+    return 0;
+}

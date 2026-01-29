f32fc433c9e15936288a8a0d6f391948c7aac723
Kaarthi_S <kaarthisenthilkumar@gmail.com>
2026-01-29 20:53:43 +0530

    Add files via upload

---
+++ pattern in nested loop/pattern3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 65; i < 65 + n; i++)
+    {
+        for (int j = 65; j <= i; j++)
+        {
+            if (i >= 'A' && i <= 'Z')
+            {
+                printf(" %c", i);
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

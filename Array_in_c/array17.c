83cbdec4b04fe924f8025a99cef7a77a7dfe2a62
Kaarthi_S <kaarthisenthilkumar@gmail.com>
2026-02-01 19:16:10 +0530

    Add files via upload

---
+++ Array_in_c/array17.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main (){
+    int n;
+    scanf("%d",&n);
+
+    int arr[n];
+    for(int i = 0 ; i<n ;i++){
+        scanf("%d",&arr[i]);
+    }
+    int palindrome = 1 ;
+    for(int i =0 ;i<n/2 ;i++){
+       if(arr[i] != arr[n-1-i]){
+        palindrome=0;
+        break;
+       }
+    }
+    if(palindrome ){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+
+    return 0;
+}

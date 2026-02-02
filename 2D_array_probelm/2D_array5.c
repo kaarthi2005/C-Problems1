0401882a7293d9d75dd6d34e24c783b983f9d2dc
Kaarthi_S <kaarthisenthilkumar@gmail.com>
2026-02-02 14:04:12 +0530

    Add files via upload

---
+++ 2D_array_probelm/2D_array5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main (){
+    int row , column ;
+    scanf("%d %d",&row ,&column);
+
+    int arr[row][column];
+
+    for(int i =0 ;i<row ;i++){
+        for(int j = 0;j<column;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+
+    int count =0 ;
+     for(int i =0 ;i<row ;i++){
+        for(int j = 0;j<column;j++){
+            if(arr[i][j]>200){
+                count++;
+            }
+        }
+    }
+    printf("Bright Pixels = %d",count);
+    
+
+    return 0;
+
+}

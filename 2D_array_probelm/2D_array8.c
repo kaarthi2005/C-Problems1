7f65f7f4fb5a745734ef152cd67a1372aa4936e1
Kaarthi_S <kaarthisenthilkumar@gmail.com>
2026-02-03 10:25:30 +0530

    Refactor unique count logic in 2D array
    Updated logic to count unique elements in each row by checking adjacent elements.

--- 2D_array_probelm/2D_array8.c
+++ 2D_array_probelm/2D_array8.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
-int main() {
+int main()
+{
     int R, C;
     scanf("%d %d", &R, &C);
     int arr[R][C];
-    for(int i=0;i<R;i++)
-        for(int j=0;j<C;j++)
-            scanf("%d",&arr[i][j]);
-
-    int maxUnique=0, deptIndex=0;
-    for(int i=0;i<R;i++) {
-        int freq[1000]={0}, unique=0; 
-        for(int j=0;j<C;j++) {
-            if(freq[arr[i][j]]==0) unique++;
-            freq[arr[i][j]]++;
-        }
-        if(unique > maxUnique) {
-            maxUnique = unique;
-            deptIndex = i;
+    for (int i = 0; i < R; i++)
+    {
+        for (int j = 0; j < C; j++)
+        {
+            scanf("%d", &arr[i][j]);
         }
     }
-    printf("%d\n", deptIndex);
+
+    int unique = 0;
+    
+    for (int i = 0; i < R; i++)
+    {
+        int count =0;
+        for (int j = 0; j < C; j++)
+        {
+            if(arr[i][j]==arr[i][j+1]){
+                count = 1;
+            }
+        }
+        if(count == 0){
+            unique++;
+        }
+
+    }
+    printf("%d\n",unique);
     return 0;
 }

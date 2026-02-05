ca77783ae7e49a2bba7254c98be5c61f6e28fd80
Kaarthi_S <kaarthisenthilkumar@gmail.com>
2026-02-05 14:10:28 +0530

    Add files via upload

--- 2D_array_probelm/2D_array8.c
+++ 2D_array_probelm/2D_array8.c
@@ -1,33 +1,71 @@
 #include <stdio.h>
+
 int main()
 {
-    int R, C;
-    scanf("%d %d", &R, &C);
-    int arr[R][C];
-    for (int i = 0; i < R; i++)
+
+    int r, c;
+    scanf("%d  %d", &r, &c);
+
+    int arr[r][c];
+    for (int i = 0; i < r; i++)
     {
-        for (int j = 0; j < C; j++)
+        for (int j = 0; j < c; j++)
         {
             scanf("%d", &arr[i][j]);
         }
     }
-
-    int unique = 0;
-    
-    for (int i = 0; i < R; i++)
+    int max = 0;
+    int sum = 0, sum1 = 0, sum2 = 0, sum3 = 0;
+    for (int i = 0; i < r - 1; i++)
     {
-        int count =0;
-        for (int j = 0; j < C; j++)
+        for (int j = 0; j < c - 1; j++)
         {
-            if(arr[i][j]==arr[i][j+1]){
-                count = 1;
-            }
+            sum += arr[i][j];
         }
-        if(count == 0){
-            unique++;
-        }
-
     }
-    printf("%d\n",unique);
+    if (sum > max)
+    {
+        max = sum;
+    }
+
+    for (int i = 0; i < r - 1; i++)
+    {
+        for (int j = 1; j < c; j++)
+        {
+            sum1 += arr[i][j];
+        }
+    }
+    if (sum1 > max)
+    {
+        max = sum1;
+    }
+
+    for (int i = 1; i < r; i++)
+    {
+        for (int j = 0; j < c - 1; j++)
+        {
+            sum2 += arr[i][j];
+        }
+    }
+    if (sum2 > max)
+    {
+        max = sum2;
+    }
+
+    for (int i = 1; i < r; i++)
+    {
+        for (int j = 1; j < c; j++)
+        {
+            sum3 += arr[i][j];
+        }
+    }
+
+    if (sum3 > max)
+    {
+        max = sum3;
+    }
+
+    printf(" max : %d \n", max);
+
     return 0;
 }

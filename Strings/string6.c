9da1010a5f4988adfc51555d59a952c821832139
Kaarthi_S <kaarthisenthilkumar@gmail.com>
2026-02-10 11:01:08 +0530

    Add files via upload

---
+++ Strings/string6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+#include <string.h>
+
+int main(){
+    char str[1000];
+    fgets(str,sizeof(str),stdin);
+    int count = 0 ;
+    int n = strlen(str);
+    for(int i=0 ;i<n ;i++){
+         if(str[i] == 'a' ||str[i] == 'e' ||str[i] == 'i' ||str[i] == 'o' ||str[i] == 'u' ||str[i] == 'A' || str[i] == 'E' ||str[i] == 'I' ||str[i] == 'O' ||str[i] == 'U'  ){
+            count++;
+         }
+    }
+    printf("%d",count);
+    return 0;
+}

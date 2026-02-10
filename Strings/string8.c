d3fa58a2d6291b52589d5f1c82cd2713cefe5a7f
Kaarthi_S <kaarthisenthilkumar@gmail.com>
2026-02-10 11:02:06 +0530

    Add files via upload

---
+++ Strings/string8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (){
+    char str[1000];
+    fgets(str,sizeof(str),stdin);
+    int n = strlen(str);
+    int count =0 ;
+    for(int i=0;i<n;i++){
+        if(str[i] == ' '){
+            count++;
+        }
+    }
+    printf("%d",count);
+
+    return 0;
+}
